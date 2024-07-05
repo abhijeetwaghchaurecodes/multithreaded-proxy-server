abfac89dcc776c4acda9400d26ad992456cf64d5
Abhijeet Waghchaure <abhijeetswaghchaure@gmail.com>
2024-07-05 16:47:11 +0530

    SERVER and CLIENT side binding
    done with binding here, from creation of the socket to binding it by accepting/rejecting connections, an error regarding [struct "sock_addr" which is unable to find the sin_* functions], i'll resolve them later

--- 003_proxy_server_with_cache.c
+++ 003_proxy_server_with_cache.c
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h>  //to handle and manage sockets
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
-#include <arpa/inet.h> //to support address related queries
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
@@ -62,6 +63,67 @@ int main(int argc, char* argv[]){
 
     printf("starting proxy server at port: %d\n", port_number);
     proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
-
     /*timestamp - 29:00*/
+    if(proxy_socketId < 0){
+        perror("failed to create a socket\n");
+        exit(1);
+    }
+    int reuse = 1;
+    if(setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0){
+        perror("setSockOpt failed\n");
+    }
+    
+    bzero((char*)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);   //assigning PORT to the PROXY
+    server_addr.sin_addr.s_addr = INADDR_ANY; //any available address assigned
+
+    // Binding the socket
+	if( bind(proxy_socketId, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0 )
+	{
+		perror("Port is not available\n");
+		exit(1);
+	}
+	printf("Binding on port: %d\n",port_number);
+
+    int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+
+    if(listen_status < 0){
+        perror("error in listening\n");
+        exit(1);
+    }
+
+    int i = 0;
+    int Connected_socketId[MAX_CLIENTS];
+    // Infinite Loop for accepting connections
+	while(1)
+	{
+		
+		bzero((char*)&client_addr, sizeof(client_addr));			// Clears struct client_addr
+		client_len = sizeof(client_addr); 
+
+        // Accepting the connections
+		client_socketId = accept(proxy_socketId, (struct sockaddr*)&client_addr,(socklen_t*)&client_len);	// Accepts connection
+		if(client_socketId < 0)
+		{
+			fprintf(stderr, "Error in Accepting connection !\n");
+			exit(1);
+		}
+		else{
+			Connected_socketId[i] = client_socketId; // Storing accepted client into array
+		}
+
+		// Getting IP address and port number of client
+		struct sockaddr_in* client_pt = (struct sockaddr_in*)&client_addr;
+		struct in_addr ip_addr = client_pt->sin_addr;
+		char str[INET_ADDRSTRLEN];										// INET_ADDRSTRLEN: Default ip address size
+		inet_ntop( AF_INET, &ip_addr, str, INET_ADDRSTRLEN );
+		printf("Client is connected with port number: %d and ip address: %s \n",ntohs(client_addr.sin_port), str);
+		//printf("Socket values of index %d in main function is %d\n",i, client_socketId);
+		pthread_create(&tid[i],NULL,thread_fn, (void*)&Connected_socketId[i]); // Creating a thread for each client accepted
+		i++; 
+	}
+	close(proxy_socketId);									// Close socket
+ 	return 0;
+    /*timestamp - 45:00*/
 }
